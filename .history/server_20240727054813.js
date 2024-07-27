8ad8458a985b15c978920302d0ef337b1761479e
Carpe Noctem <carpenoctem.connect@gmail.com>
2024-07-27 05:56:18 +0300

    asd

--- .history/server_20240727054813.js
+++ .history/server_20240727054813.js
@@ -16,26 +16,20 @@ io.on('connection', (socket) => {
   io.emit('users', users);
   io.emit('calls', calls);
   
-  console.log('A user connected. Total users:', users);
+  console.log('a user connected');
 
   socket.on('offer', (offer) => {
-    console.log('Received offer:', offer);
     calls++;
     io.emit('calls', calls);
     socket.broadcast.emit('offer', offer);
-    console.log('Offer broadcasted. Total calls:', calls);
   });
 
   socket.on('answer', (answer) => {
-    console.log('Received answer:', answer);
     socket.broadcast.emit('answer', answer);
-    console.log('Answer broadcasted.');
   });
 
   socket.on('candidate', (candidate) => {
-    console.log('Received candidate:', candidate);
     socket.broadcast.emit('candidate', candidate);
-    console.log('Candidate broadcasted.');
   });
 
   socket.on('hangup', () => {
@@ -43,16 +37,15 @@ io.on('connection', (socket) => {
       calls--;
     }
     io.emit('calls', calls);
-    console.log('Call ended. Total calls:', calls);
   });
 
   socket.on('disconnect', () => {
     users--;
     io.emit('users', users);
-    console.log('User disconnected. Total users:', users);
+    console.log('user disconnected');
   });
 });
 
 server.listen(8080, () => {
-  console.log('Listening on *:8080');
+  console.log('listening on *:8080');
 });
